971011ed65fd678a70072c1dcaf600b320043e93
Pranav Bahuguna <pranav.bahuguna@gmail.com>
2015-02-23 21:01:31 +0000

    Created a (non-functional) javascript file for the page, made some changes to the CSS hierachy regarding the logbook/entry creation buttons.

---
+++ Logbook/pages/logbook-page/scripts.js
@@ -0,0 +1,13 @@
+<script>
+
+function createLogbook ()
+{
+  var button = document.createElement ("button");
+  button.class =
+  document.body.appendChild (button);
+}
+
+function createLogbookEntry ()
+{
+
+}
