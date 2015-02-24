f41f3b1400581b0bd0af1cb4667fe85c80da0fb2
Pranav Bahuguna <pranav.bahuguna@gmail.com>
2015-02-24 20:58:57 +0000

    Added new stuff for div creation on button click

--- Logbook/pages/logbook-page/scripts.js
+++ Logbook/pages/logbook-page/scripts.js
@@ -1,3 +1,7 @@
+// Keeps track of the id number given to logbook entries.
+var idCounter = 0;
+
+// Creates a logbook button in the left selection pane.
 function createLogbook ()
 {
   var button = document.createElement ("button");
@@ -7,7 +11,28 @@ function createLogbook ()
   document.getElementById("logbookSelectionPane").appendChild (button);
 }
 
+// Creates a logbook entry in the right editor pane.
 function createLogbookEntry ()
 {
+  var entryDiv      = document.createElement ("div");
+  var entryHeader   = document.createElement ("div");
+  var entryContent  = document.createElement ("div");
+  var entryID       = "entry" + idCounter;
 
+  // Creates the container div and sets its id so the other divs can be placed
+  // inside.
+  entryDiv.setAttribute ("class", "entryContainer");
+  entryDiv.id = entryID;
+  document.getElementById("logbookEditorSpace").appendChild (entryDiv);
+
+  // Adds the inner divs inside of the new container div.
+  entryHeader.setAttribute ("class", "entryHeader");
+  entryHeader.innerHTML = "More test text";
+
+  entryContent.setAttribute ("class", "entryContent");
+  document.getElementById(entryID).appendChild (entryHeader);
+  document.getElementById(entryID).appendChild (entryContent);
+
+  // idCounter is incremented (guarantees each entry id is unique).
+  idCounter++;
 }
