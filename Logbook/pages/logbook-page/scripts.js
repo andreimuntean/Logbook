8881e1120a7f28a02acb81bd99d81e258d093d63
Pranav Bahuguna <pranav.bahuguna@gmail.com>
2015-03-01 17:09:13 +0000

    Created a popup window that appears on logbook creation or clicking logbook settings icon.

--- Logbook/pages/logbook-page/scripts.js
+++ Logbook/pages/logbook-page/scripts.js
@@ -2,6 +2,8 @@
 var logbookIDCounter = 0;
 // Keeps track of the id number given to logbook entries.
 var idCounter = 0;
+// This keeps an array of every logbook created so far.
+var logbooks = [];
 
 // Creates a logbook and settings button in the left selection pane.
 function createLogbook ()
@@ -25,7 +27,10 @@ function createLogbook ()
   // Creates the logbook settings button and adds it as well.
   settingsButton.setAttribute ("class", "settingsButton");
   settingsButton.setAttribute ("type", "button");
+  settingsButton.onclick = partial (toggleLogbookSettings, true);
   document.getElementById(logbookID).appendChild (settingsButton);
+
+  toggleLogbookSettings (true);
 
   // logbookIDCounter is incremented (guarantees each entry id is unique).
   logbookIDCounter++;
@@ -55,4 +60,29 @@ function createLogbookEntry ()
 
   // idCounter is incremented (guarantees each entry id is unique).
   idCounter++;
+}
+
+// Opens the logbook settings popup.
+function toggleLogbookSettings (isOn)
+{
+  var display = "";
+
+  if (isOn) {
+    display = 'block';
+  } else {
+    display = 'none'
+  }
+
+  document.getElementById("settings").style.display = display;
+  document.getElementById("blanket").style.display = display;
+}
+
+/* Code taken from: 'http://blog.dreasgrech.com/2009/11/passing-pointer-to-functi
+ * on-with.html'. It allows a function reference to be passed with parameters.*/
+var partial = function (func /*, 0..n args */) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return function () {
+        var allArguments = args.concat(Array.prototype.slice.call(arguments));
+        return func.apply(this, allArguments);
+    }
 }
