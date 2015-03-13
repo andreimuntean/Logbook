6fc127c95e722f87af2e4696c0f4c230d3bcd079
Sebastian Costin <ascostin21@gmail.com>
2015-03-13 01:09:12 +0000

    Added functionality for saving logbooks entries, and editing (adding content to them). Also disabled edit and create buttons while you edit other logs.

--- Logbook/js/scripts.js
+++ Logbook/js/scripts.js
@@ -1,5 +1,246 @@
 // Global utility scripts go here.
 
+function addLogbookToDB(logbookName, logbookPrivacy){
+    $.ajax({
+            url:"inc/logbook.php",
+            type:"post",
+            data:{
+                "name":logbookName,
+                "privacy":logbookPrivacy,
+                "action":"new"
+            }
+    }).done(function(response){
+        createLogbook(logbookName, response)
+            console.log(response);
+    })
+}
+
+function deleteLogbookfromDB(logbookID){
+    $.ajax({
+            url:"inc/logbook.php",
+            type:"post",
+            data:{
+                "id":logbookID,
+                "action":"delete"
+            }
+    }).done(function(response){
+            console.log(response);
+    })
+}
+
+var logbookIDCounter = 0;     // Keeps track of the id number given to logbooks.
+var idCounter = 0;            // Keeps track of the id number given to logbook entries.
+var logbooks = [];            // This keeps an array of every logbook created so far.
+var createNewLogbook = false; // Decides whether or not to create a new logbook when saving.
+var currentLogbookID = 0;
+
+// Creates a logbook and settings button in the left selection pane.
+function createLogbook (logbookName, logbookID)
+{
+  /*var logbookID       = "logbook" + logbookIDCounter;*/
+  var logbookDiv      = document.createElement ("div");
+  var logbookButton   = document.createElement ("button");
+  var settingsButton  = document.createElement ("button");
+
+  // Creates the container div for the logbook and settings button.
+  logbookDiv.setAttribute ("class", "logbookContainer");
+  logbookDiv.id = logbookID;
+  document.getElementById("logbookSelectionPane").appendChild (logbookDiv);
+
+  // Creates the logbook button and adds it to the new div.
+  logbookButton.setAttribute ("class", "logbookButton");
+  logbookButton.setAttribute ("type", "button");
+  logbookButton.innerHTML = /*"Test text";*/ logbookName;
+  document.getElementById(logbookID).appendChild (logbookButton);
+
+  // Creates the logbook settings button and adds it as well.
+  settingsButton.setAttribute ("class", "settingsButton");
+  settingsButton.setAttribute ("type", "button");
+  settingsButton.onclick = partial (openLogbookSettings, logbookID);
+  document.getElementById(logbookID).appendChild (settingsButton);
+
+  // logbookIDCounter is incremented (guarantees each entry id is unique).
+  logbookIDCounter++;
+}
+
+// Creates a logbook entry in the right editor pane.
+function createLogbookEntry ()
+{
+  document.getElementById("createLogbookEntryButton").disabled = true;
+  document.getElementById("createLogbookEntryButton").setAttribute("style", "background-color: #393F48");
+  var editArray = document.getElementsByClassName("editButton");
+  for (var editInstance = 0; editInstance < editArray.length; editInstance++)
+  {
+    editArray[editInstance].removeAttribute("onclick");
+    editArray[editInstance].setAttribute("style", "color: #9B9EA2");
+  } 
+
+  var entryDiv      = document.createElement ("div");
+  var entryHeader   = document.createElement ("div");
+  var entryContent  = document.createElement ("div");
+  var entryID       = "entry" + idCounter;
+  var entryTextArea = document.createElement ("textarea");
+  var saveEntry     = document.createElement ("span");
+
+  // Creates the container div and sets its id so the other divs can be placed
+  // inside.
+  entryDiv.setAttribute ("class", "entryContainer");
+  entryDiv.id = entryID;
+  document.getElementById("logbookEditorSpace").appendChild (entryDiv);
+
+  // Adds the inner divs inside of the new container div.
+  entryHeader.setAttribute ("class", "entryHeader");
+  entryHeader.innerHTML = "More test text";
+
+  entryContent.setAttribute ("class", "entryContent");
+  document.getElementById(entryID).appendChild (entryHeader);
+  entryHeader.id = entryID + "H";
+  document.getElementById(entryID).appendChild (entryContent);
+  entryContent.id = entryID + "C";
+
+  entryTextArea.setAttribute ("class", "entryTextArea");
+  entryTextArea.id = "currentTextArea";
+  saveEntry.setAttribute ("class", "saveButton");
+  saveEntry.id = "currentSaveButton";
+  saveEntry.setAttribute ("onclick", "saveEntry()");
+
+  entryHeader.appendChild (saveEntry);
+  entryContent.appendChild (entryTextArea);
+
+  saveEntry.innerHTML = "Save";
+
+  // idCounter is incremented (guarantees each entry id is unique).
+  idCounter++;
+}
+
+// Code that gets executed when you save an entry
+function saveEntry () 
+{
+  var editEntry = document.createElement("span");
+  var saveEntry = document.getElementById("currentSaveButton");
+  var textArea = document.getElementById("currentTextArea");
+  var textToSave = textArea.value;
+  
+  // Create an edit button.
+  saveEntry.parentNode.appendChild(editEntry);
+  editEntry.id = editEntry.parentNode.parentNode.id + "E";
+  editEntry.setAttribute("onclick", "editEntry(this.id)");
+  editEntry.setAttribute("class", "editButton");
+  editEntry.innerHTML = "Edit";
+
+  // Remove the save button.
+  document.getElementById("currentSaveButton").remove();
+
+  // Save the text inside the log.
+  textArea.remove();
+  var entryContentID = (editEntry.id).substring(0, (editEntry.id).length - 1) + "C";
+  document.getElementById(entryContentID).innerHTML += "\n" + textToSave;
+
+  // Re-enable the create log button.
+  document.getElementById("createLogbookEntryButton").disabled = false;
+  document.getElementById("createLogbookEntryButton").setAttribute("style", "background-color: #2d3239");
+  var editArray =  document.getElementsByClassName("editButton");
+  for (var editInstance = 0; editInstance < editArray.length; editInstance++)
+  {
+    editArray[editInstance].setAttribute("onclick", "editEntry(this.id)");
+    editArray[editInstance].removeAttribute("style");
+  }
+}
+
+function editEntry (buttonId)
+{
+  document.getElementById("createLogbookEntryButton").disabled = true;
+  document.getElementById("createLogbookEntryButton").setAttribute("style", "background-color: #393F48");
+  var editArray = document.getElementsByClassName("editButton");
+  for (var editInstance = 0; editInstance < editArray.length; editInstance++)
+  {
+    editArray[editInstance].removeAttribute("onclick");
+    editArray[editInstance].setAttribute("style", "color: #9B9EA2");
+  } 
+
+  var entryTextArea = document.createElement ("textarea");
+  var saveEntry     = document.createElement ("span");
+  var editEntry     = document.getElementById(buttonId);
+
+  entryTextArea.setAttribute ("class", "entryTextArea");
+  entryTextArea.id = "currentTextArea";
+  saveEntry.setAttribute ("class", "saveButton");
+  saveEntry.id = "currentSaveButton";
+  saveEntry.setAttribute ("onclick", "saveEntry()");
+
+  // Replacing edit with save.
+  editEntry.remove();
+  var entryHeaderID = buttonId.substring(0, buttonId.length - 1) + "H";
+  document.getElementById(entryHeaderID).appendChild (saveEntry);
+  saveEntry.innerHTML = "Save";
+
+  // Adding the text area.
+  var entryContentID = buttonId.substring(0, buttonId.length - 1) + "C";
+  document.getElementById(entryContentID).appendChild (entryTextArea);
+}
+
+// Opens the logbook settings popup.
+function togglePopUp (isOn, id)
+{
+  var display = "";
+  if (isOn) {
+    display = 'block';
+  } else {
+    display = 'none'
+  }
+
+  document.getElementById(id).style.display = display;
+  document.getElementById("blanket").style.display = display;
+}
+
+// Saves the desired settings for a logbook.
+function saveLogbookSettings ()
+{
+  togglePopUp (false, 'settings')
+  // If true, a logbook is created with these
+  if (createNewLogbook)
+  {
+    createNewLogbook = false;
+    name = $("#logbookName").val();
+    $("#logbookName").val("");
+    privacy = $("#visibility").val();
+    addLogbookToDB(name, privacy);
+  }
+}
+
+// Cancels logbook creation or removes a logbook from the user's logbooks.
+function deleteLogbook ()
+{
+	togglePopUp (false, 'settings')
+	// If false, the logbook is a preexisting one and must be removed.
+	// NOTE: Insert some code here that can remove a logbook from server!
+	if (!createNewLogbook)
+	{
+    deleteLogbookfromDB(currentLogbookID);
+		var logbookToDelete = document.getElementById (currentLogbookID);
+		document.getElementById ("logbookSelectionPane").removeChild (logbookToDelete)
+	}
+}
+
+// Toggles the logbook settings popup on AND sets the currentLogbookID.
+function openLogbookSettings (logbookID)
+{
+	togglePopUp (true, 'settings');
+	currentLogbookID = logbookID;
+}
+
+/* Code taken from: 'http://blog.dreasgrech.com/2009/11/passing-pointer-to-functi
+ * on-with.html'. It allows a function reference to be passed with parameters.*/
+
+  var partial = function (func /*, 0..n args */) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return function () {
+        var allArguments = args.concat(Array.prototype.slice.call(arguments));
+        return func.apply(this, allArguments);
+    }
+  }
+
+
 $(document).ready(function(){
 	$("#signUpButton").click(function(){
 		var username = $("input[name='username']").val();
@@ -91,167 +332,4 @@ $(document).ready(function(){
       }
     })
   })
-
-
-
-})
-
-function addLogbookToDB(logbookName, logbookPrivacy){
-    $.ajax({
-            url:"inc/logbook.php",
-            type:"post",
-            data:{
-                "name":logbookName,
-                "privacy":logbookPrivacy,
-                "action":"new"
-            }
-    }).done(function(response){
-        createLogbook(logbookName, response)
-            console.log(response);
-    })
-}
-
-function deleteLogbookfromDB(logbookID){
-    $.ajax({
-            url:"inc/logbook.php",
-            type:"post",
-            data:{
-                "id":logbookID,
-                "action":"delete"
-            }
-    }).done(function(response){
-            console.log(response);
-    })
-}
-
-var logbookIDCounter = 0;     // Keeps track of the id number given to logbooks.
-var idCounter = 0;            // Keeps track of the id number given to logbook entries.
-var logbooks = [];            // This keeps an array of every logbook created so far.
-var createNewLogbook = false; // Decides whether or not to create a new logbook when saving.
-var currentLogbookID = 0;
-
-// Creates a logbook and settings button in the left selection pane.
-function createLogbook (logbookName, logbookID)
-{
-  /*var logbookID       = "logbook" + logbookIDCounter;*/
-  var logbookDiv      = document.createElement ("div");
-  var logbookButton   = document.createElement ("button");
-  var settingsButton  = document.createElement ("button");
-
-  // Creates the container div for the logbook and settings button.
-  logbookDiv.setAttribute ("class", "logbookContainer");
-  logbookDiv.id = logbookID;
-  document.getElementById("logbookSelectionPane").appendChild (logbookDiv);
-
-  // Creates the logbook button and adds it to the new div.
-  logbookButton.setAttribute ("class", "logbookButton");
-  logbookButton.setAttribute ("type", "button");
-  logbookButton.innerHTML = /*"Test text";*/ logbookName;
-  document.getElementById(logbookID).appendChild (logbookButton);
-
-  // Creates the logbook settings button and adds it as well.
-  settingsButton.setAttribute ("class", "settingsButton");
-  settingsButton.setAttribute ("type", "button");
-  settingsButton.onclick = partial (openLogbookSettings, logbookID);
-  document.getElementById(logbookID).appendChild (settingsButton);
-
-  // logbookIDCounter is incremented (guarantees each entry id is unique).
-  logbookIDCounter++;
-}
-
-// Creates a logbook entry in the right editor pane.
-function createLogbookEntry ()
-{
-  var entryDiv      = document.createElement ("div");
-  var entryHeader   = document.createElement ("div");
-  var entryContent  = document.createElement ("div");
-  var entryID       = "entry" + idCounter;
-  var entryTextArea = document.createElement ("textarea");
-  var saveEntry     = document.createElement ("span");
-
-  // Creates the container div and sets its id so the other divs can be placed
-  // inside.
-  entryDiv.setAttribute ("class", "entryContainer");
-  entryDiv.id = entryID;
-  document.getElementById("logbookEditorSpace").appendChild (entryDiv);
-
-  // Adds the inner divs inside of the new container div.
-  entryHeader.setAttribute ("class", "entryHeader");
-  entryHeader.innerHTML = "More test text";
-
-  entryContent.setAttribute ("class", "entryContent");
-  document.getElementById(entryID).appendChild (entryHeader);
-  document.getElementById(entryID).appendChild (entryContent);
-
-  entryTextArea.setAttribute ("class", "entryTextArea");
-  saveEntry.setAttribute ("class", "saveButton");
-
-  entryHeader.appendChild (saveEntry);
-  entryContent.appendChild (entryTextArea);
-
-  saveEntry.innerHTML = "Save";
-
-  // idCounter is incremented (guarantees each entry id is unique).
-  idCounter++;
-}
-
-// Opens the logbook settings popup.
-function togglePopUp (isOn, id)
-{
-  var display = "";
-  if (isOn) {
-    display = 'block';
-  } else {
-    display = 'none'
-  }
-
-  document.getElementById(id).style.display = display;
-  document.getElementById("blanket").style.display = display;
-}
-
-// Saves the desired settings for a logbook.
-function saveLogbookSettings ()
-{
-  togglePopUp (false, 'settings')
-  // If true, a logbook is created with these
-  if (createNewLogbook)
-  {
-    createNewLogbook = false;
-    name = $("#logbookName").val();
-    $("#logbookName").val("");
-    privacy = $("#visibility").val();
-    addLogbookToDB(name, privacy);
-  }
-}
-
-// Cancels logbook creation or removes a logbook from the user's logbooks.
-function deleteLogbook ()
-{
-	togglePopUp (false, 'settings')
-	// If false, the logbook is a preexisting one and must be removed.
-	// NOTE: Insert some code here that can remove a logbook from server!
-	if (!createNewLogbook)
-	{
-    deleteLogbookfromDB(currentLogbookID);
-		var logbookToDelete = document.getElementById (currentLogbookID);
-		document.getElementById ("logbookSelectionPane").removeChild (logbookToDelete)
-	}
-}
-
-// Toggles the logbook settings popup on AND sets the currentLogbookID.
-function openLogbookSettings (logbookID)
-{
-	togglePopUp (true, 'settings');
-	currentLogbookID = logbookID;
-}
-
-/* Code taken from: 'http://blog.dreasgrech.com/2009/11/passing-pointer-to-functi
- * on-with.html'. It allows a function reference to be passed with parameters.*/
-
-  var partial = function (func /*, 0..n args */) {
-    var args = Array.prototype.slice.call(arguments, 1);
-    return function () {
-        var allArguments = args.concat(Array.prototype.slice.call(arguments));
-        return func.apply(this, allArguments);
-    }
-  }
+}) 
